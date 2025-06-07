35dadac9454072e1d45577028ac8916df01d8358
wjy030522 <1357253688@qq.com>
2025-06-07 17:28:44 +0800

    update

--- README.md
+++ README.md
@@ -1,13 +1,14 @@
 # object_detect
 
 ## 环境
-- Python 3.6  
-- 具体依赖包版本请参考 `src/YOLOv5-Lite/requirements.txt`
+- Python 3.8.20 
+- 具体依赖包版本请参考 `src/requirements.txt`
 
 ## 执行步骤
 1. 进入项目目录：
     ```bash
-    cd src/YOLOv5-Lite
+    cd src/
+    pip install -r requirements.txt
     ```
 
 2. 修改 `process.py` 文件中的图片目录路径，确保指向你的图片所在位置。
---
+++ src/process.py
@@ -0,0 +1,29 @@
+import os
+import time
+
+from ultralytics import YOLO
+
+model = YOLO(model='best.pt')
+
+
+def process_img(img_path):
+    # 执行预测
+    results = model.predict(source=img_path, save=False, show=False, verbose=False)
+
+    output_list = []
+
+    # 通常每张图片返回一个结果
+    for result in results:
+        boxes = result.boxes
+        for box in boxes:
+            xyxy = box.xyxy[0].tolist()  # [x1, y1, x2, y2]
+            x1, y1, x2, y2 = map(int, xyxy)
+            w = x2 - x1
+            h = y2 - y1
+            output_list.append({"x": x1, "y": y1, "w": w, "h": h})
+
+    return output_list
+if __name__ == '__main__':
+    img_path = '1.jpg'  # 替换为你的图片路径
+    result = process_img(img_path)
+    print(result)
